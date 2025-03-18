10f5bdfe1ffe3e402ad9d339d09da463587b1125
Augusto Barros <126268126+augusto-barros@users.noreply.github.com>
2025-03-18 17:59:17 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -41,7 +41,7 @@ export default function Home() {
 
   return (
     <main className="bg-gray-900 text-gray-100 min-h-screen flex flex-col items-center p-8">
-      <h1 className="text-4xl font-bold mb-6">Fake Spotify Artist Detector</h1>
+      <h1 className="text-4xl font-bold mb-6">Spotify AI Artist Detector</h1>
       <div className="flex flex-col items-center w-full max-w-md">
         <input
           type="text"
